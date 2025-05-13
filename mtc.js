fbfcc610430dfb24b0ace657fe6adf8e60002fcf
rethinkzero <93017797+rethinkzero@users.noreply.github.com>
2025-05-13 12:50:06 -0400

    Create mtc.js

---
+++ mtc.js
@@ -0,0 +1,23 @@
+
+webix.ui({
+  rows:[
+    {id:"col-layout", cols:[
+      { view:"fieldset", label:"Test Fieldset",
+          body:{
+              rows:[
+                  {view:"text", width:200, label:"test"}
+              ]
+          }
+      },
+      { view:"fieldset", label:"Test Fieldset",
+          body:{
+              rows:[
+                  {view:"text", width:200, label:"test"}
+              ]
+          }
+      },
+         
+    ]}
+
+  ]
+});
